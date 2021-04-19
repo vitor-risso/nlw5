732876565ef9afcb0994be885ec08ba2ee782e0f
vitor-risso <vitor.risso13@gmail.com>
2021-04-19 18:50:36 -0300

    [001] Finishing first class, creating 2 routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express()
+
+app.get('/', (req: Request, res: Response) => { res.json({ "message": " ola" }) })
+
+app.post('/', (req: Request, res: Response) => {
+  res.json({"message": "Deu tudo certo"})
+})
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080")
