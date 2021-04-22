52807a994b57f3b461b4c5eac4c615fd8a0b793b
vitor-risso <vitor.risso13@gmail.com>
2021-04-22 16:06:02 -0300

    [010] adding public and starting ws and http protocol

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response, urlencoded } from 'express';
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+
 import './database/index'
 import { routes } from './routes'
 
 const app = express()
 
+const http = createServer(app) // creating a htpp protocol
+const io = new Server(http) // creating websocket protocol
+
+io.on("connection", (socket: Socket) => {
+  console.log(socket.id)
+})
+
 app.use(express.json())
 app.use(routes)
 
-app.listen(8080, () => {
+http.listen(8080, () => {
   console.log("Server is running on port 8080")
 })
